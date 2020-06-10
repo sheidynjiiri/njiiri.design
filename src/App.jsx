71282d44b9d019481d3c5a2ff330cb638a9e8edc
Denzel Wamburu <denzjoseph@gmail.com>
2020-06-10 04:53:53 +0300

    chore(*): Add contacts

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
                         'https://firebasestorage.googleapis.com/v0/b/jechercheundev.appspot.com/o/technologies%2Ftechnologies_list.json?alt=media&token=459028ba-d9bc-4480-a3c4-88633afab7e2'
                 },
                 // dismissFooter : true
-                // showContactInfos: true,
+                showContactInfos: true,
                 customization,
                 maxCardsPerRow: 3
             }}
