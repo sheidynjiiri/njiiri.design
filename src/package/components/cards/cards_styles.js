ccf279e6ce9218399a9e6e75e32e59049bc65b11
Denzel Wamburu <denzjoseph@gmail.com>
2020-06-10 04:10:15 +0300

    feat(*): 🖨️

---
+++ src/package/components/cards/cards_styles.js
@@ -0,0 +1,34 @@
+import { createScreenWidthMediaQuery } from '../../utils/styles/styles_utils';
+
+export const styles = (theme) => {
+    const {
+        miscellaneous: { spacing },
+        components: {
+            cards: { width: cardWidth }
+        }
+    } = theme;
+
+    return {
+        container: {
+            width: '100%',
+            marginTop: -(spacing * 12),
+            display: 'flex',
+            justifyContent: 'center',
+            position: 'relative',
+            zIndex: 3
+        },
+        cards: ({ maxCardsPerRow }) => ({
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            ...(maxCardsPerRow && {
+                maxWidth: (cardWidth + spacing * 2 * 2) * maxCardsPerRow
+            })
+        }),
+        [createScreenWidthMediaQuery('max-width', theme.screenSizes.medium)]: {
+            cards: {
+                maxWidth: '95% !important'
+            }
+        }
+    };
+};
